b65a6c65b314646b7c8ca5717557d909705e4286
Gabby <ortmangm@iastate.edu>
2015-04-04 17:07:35 -0500

    time.sleep change. 59 --> 3

--- timelapse_test.py
+++ timelapse_test.py
@@ -8,7 +8,7 @@ time.sleep(1) # Camera warm-up time
         print('Captured %s' % filename)
         # Capture one image a minute
         #decrease the time.sleep argument to take more images
-        time.sleep(59)
+        time.sleep(3)
         #this conditional will stop the iterator once 60 images have been taken, this can be changed too if you want. 
         if i == 59:
             break
